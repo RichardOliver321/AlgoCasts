400e771cfcdc0013c06b29b7473221c64ae0cdaa
Richard_Oliver <richard.oliver@mmiholdings.co.za>
2020-01-06 09:14:07 +0200

    Reverse int

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -9,7 +9,6 @@
 
 
 function palindrome(str) {
-
     return str.split('').every((char, i) => {
         return char === str[str.length - i - 1];
     });
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let revstr = n.toString().split('').reduce((rev, char) => {
+        // if (char != '-'){ 
+            // return char+rev
+        // } javascript doesn't care that your number is: 51- it can still convert /shrug
+        return char + rev
+    });
+
+    return parseInt(revstr) * Math.sign(n)
+
+}
 
 module.exports = reverseInt;
