8cbf57661492663ad1b46a6d297a714eb80b7a6f
Richard_Oliver <richard.oliver@mmiholdings.co.za>
2020-01-06 09:00:01 +0200

    Palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,12 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return reversed(str) == str
+}
+
+function reversed(str) {
+    return str.split('').reduce((rev, c) => c + rev, '');
+}
 
 module.exports = palindrome;
