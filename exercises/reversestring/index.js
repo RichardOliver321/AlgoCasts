ef957abb741b39b6f285ba675c26dd3a85cdabed
Richard_Oliver <richard.oliver@mmiholdings.co.za>
2020-01-06 08:40:50 +0200

    Reversed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed
+    }, '');
+ }
 
+//  function reverse(str) {
+//     return str
+//     .split("")
+//     .reverse()
+//     .join("")
+//  }
+  
+// function reverse(str) {
+//     let reversed = "";
+//     for (let char of str) {
+//         reversed = char + reversed
+//     }
+//     return reversed
+// }
 module.exports = reverse;
