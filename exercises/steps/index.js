dc25c742a075532a3ec200cb7115e682b127dd9c
Richard_Oliver <richard.oliver@mmiholdings.co.za>
2020-01-06 13:37:32 +0200

    steps recursive

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,41 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    for(let row = 0; row < n; row++){
-        let stair = ""
+function steps(n, row = 0, stair = "") {
+    if(n === row) {
+        return;
+    }        
 
-        for(let column = 0; column < n; column++){
-            if(column <= row) {
-                stair += "#";
-            } else {
-                stair += " ";
-            }
-        }
+    if(n === stair.length) {
         console.log(stair)
+        steps(n, row + 1)
+        return;
     }
+
+    if(stair.length <= row) {
+        stair += '#'
+    } else {
+        stair += ' '
+    }
+ 
+    steps(n, row, stair)
 }
 
 module.exports = steps;
+
+
+
+// function steps(n) {
+//     for(let row = 0; row < n; row++){
+//         let stair = ""
+
+//         for(let column = 0; column < n; column++){
+//             if(column <= row) {
+//                 stair += "#";
+//             } else {
+//                 stair += " ";
+//             }
+//         }
+//         console.log(stair)
+//     }
+// }
